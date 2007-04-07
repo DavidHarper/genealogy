a04f3adcb1024a67d3c87b871bc6265942c4bddf
David Harper <david@obliquity.net>
2007-04-07 16:06:42 +0000

    Added constant to represent the absence of a line number.
    git-svn-id: file:///disk2/repository/svn/trunk/genealogy@363 32807195-9110-437f-9ebb-0d7c4c47859f

--- com/obliquity/genealogy/gedcom/GedcomException.java
+++ com/obliquity/genealogy/gedcom/GedcomException.java
@@ -1,6 +1,8 @@
 package com.obliquity.genealogy.gedcom;
 
 public class GedcomException extends Exception {
+	public static final int NO_LINE_NUMBER = -1;
+	
 	protected int linenumber;
 	
 	public GedcomException(String message, int linenumber) {
