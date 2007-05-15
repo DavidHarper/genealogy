03f06d66ea2f7a811a74293c91b5d3be78050cbb
David Harper <david@obliquity.net>
2007-05-15 13:32:38 +0000

    Added import to reflect the new location of PersonPanel.
    git-svn-id: file:///disk2/repository/svn/trunk/genealogy@445 32807195-9110-437f-9ebb-0d7c4c47859f

--- com/obliquity/genealogy/test/peopleviewer/FamilyLister.java
+++ com/obliquity/genealogy/test/peopleviewer/FamilyLister.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import com.obliquity.genealogy.test.PersonPanel;
+
 import java.awt.BorderLayout;
 import java.util.Vector;
 import java.util.Collections;
